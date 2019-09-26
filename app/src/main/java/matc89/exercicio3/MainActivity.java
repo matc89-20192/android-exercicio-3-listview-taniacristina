1370ac317b3f979c4b816f46252a9b0e63dec701
taniacristina <roc.ta.cris@gmail.com>
2019-09-26 06:31:32 -0300

    Adicionando dados ao ListView

--- app/src/main/java/matc89/exercicio3/MainActivity.java
+++ app/src/main/java/matc89/exercicio3/MainActivity.java
@@ -3,6 +3,7 @@ package matc89.exercicio3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -34,17 +35,30 @@ public class MainActivity extends AppCompatActivity {
 
     // Função do botão adicionar
     public void clicouAdicionar(View v) {
-        String textoDescricao = editTextDescricao.getText().toString();
+        String descricao = editTextDescricao.getText().toString();
         int prioridade = Integer.parseInt(editTextPrioridade.getText().toString());
 
         // Validando dados
         if(prioridade < 1 || prioridade > 10) {
             Toast.makeText(getApplicationContext(),"A prioridade deve estar entre 1 e 10.", Toast.LENGTH_SHORT).show();
         } else {
-            
+            Tarefa tarefa = new Tarefa(descricao,prioridade);
+            if(tarefas.contains(tarefa)) {
+                Toast.makeText(getApplicationContext(),"Tarefa já cadastrada.", Toast.LENGTH_SHORT).show();
+                editTextDescricao.setText("");
+                editTextPrioridade.setText("");
+            } else {
+                tarefas.add(tarefa);
+                editTextDescricao.setText("");
+                editTextPrioridade.setText("");
+                ArrayAdapter<Tarefa> adapter = new ArrayAdapter<Tarefa>(this,android.R.layout.simple_list_item_1, tarefas);
+                listView.setAdapter(adapter);
+            }
         }
 
-        System.out.println("Descrição: " +textoDescricao);
+        System.out.println("Descrição: " +descricao);
         System.out.println("Prioridade: " +prioridade);
     }
+
+
 }
--- app/src/main/java/matc89/exercicio3/Tarefa.java
+++ app/src/main/java/matc89/exercicio3/Tarefa.java
@@ -16,4 +16,19 @@ public class Tarefa {
     public int getPrioridade() {
         return prioridade;
     }
+
+    // Metódo para verificar se a descrição já existe
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Tarefa)) {
+            return false;
+        }
+        final Tarefa other = (Tarefa) obj;
+        return this.getDescricao().equals(other.getDescricao());
+    }
+
+    @Override
+    public String toString() {
+        return descricao + "  " + "Prioridade: " + prioridade;
+    }
 }
